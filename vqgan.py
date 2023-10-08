a6b57327549d1d3321665abe5c20e1282260d875
Dim <cibershaman@gmail.com>
2023-10-08 21:02:21 +0300

    Update vqgan.py

--- vqgan.py
+++ vqgan.py
@@ -67,13 +67,9 @@ def custom_to_pil(x):
   return x
 
 def VQGAN(Model = "f16_1024"):
-  if os.path.exists(f'gia/models/vqgan_imagenet_{Model}'):
-    vq_conf = load_config(f"gia/models/vqgan_imagenet_{Model}/configs/model.yaml", display=False)
-    vq_model = load_vqgan(vq_conf, ckpt_path=f"gia/models/vqgan_imagenet_{Model}/ckpts/last.ckpt")
-  else:  
-    download_model(Model)
-    vq_conf = load_config(f"chk_points/vqgan_imagenet_{Model}/configs/model.yaml", display=False)
-    vq_model = load_vqgan(vq_conf, ckpt_path=f"chk_points/vqgan_imagenet_{Model}/ckpts/last.ckpt")
+  download_model(Model)
+  vq_conf = load_config(f"chk_points/vqgan_imagenet_{Model}/configs/model.yaml", display=False)
+  vq_model = load_vqgan(vq_conf, ckpt_path=f"chk_points/vqgan_imagenet_{Model}/ckpts/last.ckpt")
   return vq_model
 
 # usage Example:
